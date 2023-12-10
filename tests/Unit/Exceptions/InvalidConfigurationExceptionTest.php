de8a03ab545b088c823dfc47886bc50142853b38
Talpx1 <simo3401@gmail.com>
2023-12-10 16:16:32 +0100

    Tested InvalidConfigurationException

--- tests/Unit/Exceptions/InvalidConfigurationExceptionTest.php
+++ tests/Unit/Exceptions/InvalidConfigurationExceptionTest.php
@@ -10,10 +10,22 @@ use Spin8\Tests\TestCase;
 #[CoversClass(InvalidConfigurationException::class)]
 final class InvalidConfigurationExceptionTest extends TestCase {
 
+        #[Test]
+    public function test_it_inserts_configuration_name_in_message(): void {        
+        $exception = new InvalidConfigurationException("Test");
+
+        $this->assertEquals("Test is not a valid Spin8 configuration.", $exception->getMessage());
+    }
+
     #[Test]
-    public function test_test(): void {        
-        //TODO
-        $this->assertTrue(true);
+    public function test_it_passes_arguments_to_parent(): void {   
+        $prev = new \LogicException();
+                
+        $exception = new InvalidConfigurationException("Test", 1, $prev);
+
+        $this->assertEquals("Test is not a valid Spin8 configuration.", $exception->getMessage());
+        $this->assertEquals(1, $exception->getCode());
+        $this->assertSame($prev, $exception->getPrevious());
     }
 
 }
