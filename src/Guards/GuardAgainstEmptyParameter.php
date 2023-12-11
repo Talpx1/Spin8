8dedc19181627af0c94c0b8ffc83a15b7ec41ef0
Talpx1 <simo3401@gmail.com>
2023-12-11 19:44:48 +0100

    small changes to GuardAgainstEmptyParameter

--- src/Guards/GuardAgainstEmptyParameter.php
+++ src/Guards/GuardAgainstEmptyParameter.php
@@ -1,37 +1,38 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Spin8\Guards;
 
 use InvalidArgumentException;
 
-final class GuardAgainstEmptyParameter{
-
+final class GuardAgainstEmptyParameter {
     /**
      * Assure that the passed parameter is not empty
      *
      * @throws InvalidArgumentException
      */
-    public static function check(mixed $parameter_to_check, bool $allow_null = false): void {
-        if(is_numeric($parameter_to_check)) {
+    public static function check(mixed $parameter_to_check, bool $allow_null = false) : void {
+        if (is_numeric($parameter_to_check)) {
             return;
         }
 
-        if(is_null($parameter_to_check) && $allow_null) {
+        if (is_null($parameter_to_check) && $allow_null) {
             return;
         }
 
-        if(is_bool($parameter_to_check)) {
+        if (is_bool($parameter_to_check)) {
             return;
         }
-        
-        if(empty($parameter_to_check)) {
+
+        if (empty($parameter_to_check)) {
             $caller = debug_backtrace()[1];
 
-            $message  = "function '{$caller['function']}' was called with non-allowed empty argument";
-            $message  .= array_key_exists('file', $caller) ? " in {$caller['file']}" : "";
-            $message  .= array_key_exists('line', $caller) ? " on line {$caller['line']}" : "";
-            $message  .= ".".PHP_EOL;
-            $message  .= array_key_exists('args', $caller) ? PHP_EOL."Passed arguments:".PHP_EOL.print_r($caller['args'], true) : "";
+            $message = "function '{$caller['function']}' was called with non-allowed empty argument";
+            $message .= array_key_exists('file', $caller) ? " in {$caller['file']}" : '';
+            $message .= array_key_exists('line', $caller) ? " on line {$caller['line']}" : '';
+            $message .= '.' . PHP_EOL;
+            $message .= array_key_exists('args', $caller) ? PHP_EOL . 'Passed arguments:' . PHP_EOL . \Safe\json_encode($caller['args']) : '';
 
             throw new InvalidArgumentException($message);
         }
