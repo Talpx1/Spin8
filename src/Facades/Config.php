9af5a8bc625eb03c6ceb9659637516b77fb7dbe6
Talpx1 <simo3401@gmail.com>
2023-12-11 18:51:14 +0100

    improved PHPDoc of Config facade

--- src/Facades/Config.php
+++ src/Facades/Config.php
@@ -1,8 +1,12 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Spin8\Facades;
 
 /**
+ * @mixin \Spin8\Configs\ConfigRepository
+ *
  * @method static mixed get(string $file_name, string $config_key)
  * @method static void set(string $file_name, string $config_key, mixed $value)
  * @method static bool has(string $file_name, string $config_key)
@@ -10,15 +14,14 @@ namespace Spin8\Facades;
  * @method static mixed getOr(string $file_name, string $config_key, mixed $default = null)
  * @method static void clear()
  *
- * @see \Spin8\Configs\Facades\Config
+ * @see \Spin8\Configs\ConfigRepository
+ * @see \Spin8\Facades\Facade
  */
 final class Config extends Facade {
-
     /** @var string[] $allowed */
-    protected static array $allowed = ["get", "set", "has", "fileLoaded", "getOr", "clear"];
+    protected static array $allowed = ['get', 'set', 'has', 'fileLoaded', 'getOr', 'clear'];
 
-    protected static function implementor(): string {
+    protected static function implementor() : string {
         return 'config';
     }
-
 }
