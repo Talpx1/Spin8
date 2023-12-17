1b6228d3b7e5d495289a07766d3e42e4c468ddb9
Talpx1 <simo3401@gmail.com>
2023-12-17 13:43:09 +0100

    small improvement to test bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
-use Spin8\Spin8;
 
 $autoloader = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloader) && is_readable($autoloader)) {
-    require_once $autoloader;
-} else {
+
+if (!file_exists($autoloader) || !is_readable($autoloader)) {
     throw new RuntimeException("[Spin8] Can't require the autoloader in ".basename(__FILE__).", it's either missing or non-readable. Check the autoloader in {$autoloader}");
 }
+
+require_once $autoloader;
 
 //WP_Mock::activateStrictMode();
 WP_Mock::setUsePatchwork(FALSE); //not using patchwork because it breaks phpunit :(
