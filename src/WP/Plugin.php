dc2cabbad8b97efc58a2282054434c388737e5d0
Talpx1 <simo3401@gmail.com>
2024-02-10 22:26:36 +0100

    fixes in Plugin class

--- src/WP/Plugin.php
+++ src/WP/Plugin.php
@@ -15,11 +15,11 @@ class Plugin {//TODO: test
             return;
         }
 
-        if (!version_compare(PHP_VERSION, config('enviroment', 'min_php_version'), '>=')) {
+        if (!version_compare(PHP_VERSION, config('environment', 'min_php_version'), '>=')) {
             throw new RuntimeException(sprintf(__('In order to run this plugin, PHP version %s (or higher) is required. Your current PHP version is %s. Please update PHP.'), config('environment', 'min_php_version'), PHP_VERSION));
         }
 
-        if (!version_compare(get_bloginfo('version'), config('environment', 'min_wordpress_version'), '>=')) {
+        if (!version_compare(wpVersion(), config('environment', 'min_wordpress_version'), '>=')) {
             throw new RuntimeException(sprintf(__('In order to run this plugin, WordPress version %s (or higher) is required. Your current WordPress version is %s. Please update WordPress.'), config('environment', 'min_wordpress_version'), wpVersion()));
         }
 
