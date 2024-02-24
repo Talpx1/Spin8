968c5de15c07c5f07631976733ac2223c060cabc
Talpx1 <simo3401@gmail.com>
2024-02-24 01:23:41 +0100

    modified Facade base class: added support for child facade mixins, improved allowed check, implementor method is now abstract

--- src/Facades/Facade.php
+++ src/Facades/Facade.php
@@ -2,27 +2,33 @@
 
 namespace Spin8\Facades;
 use BadMethodCallException;
-use RuntimeException;
 
 abstract class Facade {
 
-    /** @var string[] $allowed */
+    /** @var string[] */
     protected static array $allowed = [];
+    protected static bool $implementor_uses_mixins = false; //TODO: test
 
-    protected static function implementor(): string {
-        throw new RuntimeException('The implementor method is intended to be overridden in the child facade, to reference the class that implements the methods proxied by the facade.');
-    }
+    /**
+     * This method must return a class-string reference to the class that implements the methods proxied by the facade.
+     * This reference is resolved by the container, so is also possible to pass an alias.
+     */
+    abstract protected static function implementor(): string;
     
     /** @param mixed[] $args */
     public static function __callStatic(string $method, array $args): mixed {
         $implementor_instance  = container()->get(static::implementor());
 
-        if(! method_exists($implementor_instance, $method) || !in_array($method, static::$allowed)) {
+        if((!static::$implementor_uses_mixins && !method_exists($implementor_instance, $method)) || !self::isMethodAllowed($method)) {
             throw new BadMethodCallException(static::class . " does not have a method called {$method}");
         }
 
         // @phpstan-ignore-next-line
         return call_user_func_array([$implementor_instance, $method], $args);
+    }
+
+    private static function isMethodAllowed(string $method): bool{ //TODO: test
+        return in_array($method, static::$allowed) || in_array('*', static::$allowed);
     }
 
 }
